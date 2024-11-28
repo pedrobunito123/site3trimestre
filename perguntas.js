84e760d841d3eebdb41e81ad4e16fe3d195396ab
pedrobunito123 <pedro.kanarski@escola.pr.gov.br>
2024-11-27 22:06:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+
+criaCartao(
+    'Geografia',
+    'Em que continente está o deserto do Saara?',
+    'África'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano ocorreu a independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto é três ao cubo?',
+    '27'
+)
+
+criaCartao(
+    'Ciências',
+    'O que são os músculos estriados esqueléticos?',
+    'Músculos que controlam os movimentos voluntários'
+)
+criaCartao(
+    'Português',
+    'Qual é a diferença entre "por que" e "porque"?',
+    '"Por que" é usado em perguntas, "porque" em respostas ou explicações'
+)
+criaCartao(
+    'Inglês',
+    'Como se pergunta "Onde você mora?" em inglês?',
+    'Where do you live?'
+)
